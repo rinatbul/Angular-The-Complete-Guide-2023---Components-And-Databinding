8f45f846b157836f8ca2c0a0a1e237c459dbf844
rinatbul <rinatbuldybayev@gmail.com>
2024-04-20 19:15:03 +0500

    encapsulation: ViewEncapsulation.Emulated

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit{
   @Input('srvElement') element: {type: string, name:string, content:string}
